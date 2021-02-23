e7fcf0d2c806b2ba96c02a9e9a304df9f550ca80
vedant shete <79532555+Vedantshete41@users.noreply.github.com>
2021-02-23 23:51:36 +0530

    Assignment day 1 submitted

---
+++ Day 1/Assignment day 1 Q(2).py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = int(input('enter the val1: '))
+y= int(input('enter the val2'))
+
+print(x**y)
+
+
+# In[ ]:
+
+
+
+
---
+++ Day 1/assignment day1 Q(1).py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+x = int(input('enter the val1: '))
+y= int(input('enter the val2'))
+
+print(x+y, x-y, x*y, x/y)
+
+
+# In[ ]:
+
+
+
+
